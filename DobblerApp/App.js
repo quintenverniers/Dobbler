6f692b51c3cffdc91990ae32ad6d1294944953d9
Quinten Verniers <quinten@Quintens-MacBook-Air.local>
2018-04-06 16:48:44 +0200

    Changes

--- DobblerApp/App.js
+++ DobblerApp/App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      number: 'Welcome',
+      number: 'Test',
       color: 'white',
     }
   }
